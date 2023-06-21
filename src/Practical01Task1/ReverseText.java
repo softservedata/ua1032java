f78cd56f7b2a84dad8d9548f5ffe4e90c4d91dd2
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-06-22 02:03:12 +0300

    Practical01 Task1

---
+++ src/Practical01Task1/ReverseText.java
@@ -0,0 +1,24 @@
+package Practical01Task1;
+
+
+
+import java.util.Scanner;
+
+public class ReverseText {
+    public static void main ( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input three strings: ");
+
+        String answer1 = sc.next();
+        String answer2 = sc.next();
+        String answer3 = sc.next();
+
+        System.out.println("Output text:");
+        System.out.println(answer3);
+        System.out.println(answer2);
+        System.out.println(answer1);
+
+        sc.close();
+    }
+}
