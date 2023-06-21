c2d62c9dd44d95de57d8dfdc32d7edcce90a56b1
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-06-22 02:26:02 +0300

    Practical01 Task3

---
+++ src/Practical01Task3/Solving.java
@@ -0,0 +1,22 @@
+package Practical01Task3;
+
+import java.util.Scanner;
+
+public class Solving {
+    public static void main ( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input two numbers: ");
+
+        int a = Integer.parseInt(sc.next());
+        int b = Integer.parseInt(sc.next());
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        sc.close();
+    }
+}
