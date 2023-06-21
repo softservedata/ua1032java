08877fb9ee63bfc5c0f063e6de9d141602d29716
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-06-22 01:00:29 +0300

    Home work1 Task2

---
+++ src/HomeWork01Task2/NameAndAddress.java
@@ -0,0 +1,23 @@
+package HomeWork01Task2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NameAndAddress {
+
+    public static void main ( String[] args ) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("What is your name? ");
+        String name = br.readLine();
+
+        System.out.print("Where do you live, " + name + "? ");
+        String address = br.readLine();
+
+        System.out.println("Name is " + name + ". Address is " + address + ".\n");
+
+        BufferedReader close;
+
+    }
+}
