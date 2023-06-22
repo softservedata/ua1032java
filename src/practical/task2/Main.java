d2985406eaaf34c1d389e710f3d89a2a227c3ec6
Olexandr Mozil <sashamozil@gmail.com>
2023-06-22 03:35:40 +0300

    Added Practical Task 2

---
+++ src/practical/task2/Main.java
@@ -0,0 +1,20 @@
+// Practical 2
+package practical.task2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main (String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Type three numbers: ");
+        System.out.print("First number: ");
+        int first = Integer.parseInt(br.readLine());
+        System.out.print("Second number: ");
+        int second = Integer.parseInt(br.readLine());
+        System.out.print("Third number: ");
+        int third = Integer.parseInt(br.readLine());
+        System.out.print("Average: " + ((first+second+third)/3.0));
+    }
+}
