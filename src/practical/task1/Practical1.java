5f2cf7de5936f694422548ab8f29176f3e85b755
Linus Torvalds <hahaha@gmail.com>
2023-06-22 03:18:06 +0300

    Added Practical Task 1

---
+++ src/practical/task1/Practical1.java
@@ -0,0 +1,17 @@
+// Practical 1
+package practical.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practical1 {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Type three words: ");
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        String s3 = br.readLine();
+        System.out.print("Reversed text:\n" + s3 + "\n" + s2 + "\n" + s1);
+    }
+}
