acd14afdd038edaa5156d0a6b425887a0de61137
yakomura <yakomura1272@gmail.com>
2023-06-24 21:32:45 +0300

    homework01. Task03

---
+++ src/com/softserve/hm01/Task03.java
@@ -0,0 +1,30 @@
+package com.softserve.hm01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task03 {
+    public static void main(String[] args) throws IOException {
+        int a,b,c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //
+        System.out.print("a =");
+        a = Integer.parseInt(br.readLine());
+        System.out.print("b =");
+        b = Integer.parseInt(br.readLine());
+        //
+        c = a + b;
+        System.out.println("a + b = " + c);
+        c = a - b;
+        System.out.println("a - b = " + c);
+        c = a * b;
+        System.out.println("a * b = " + c);
+        c = a / b;
+        System.out.println("a / b = " + c);
+        c = a % b;
+        System.out.println("a % b = " + c);
+        //
+        br.close();
+    }
+}
