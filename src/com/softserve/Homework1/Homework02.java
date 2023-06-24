acb11fe8823e351927f87c0c4d852363031827df
ViGo <goliakvika22@gmail.com>
2023-06-24 17:16:24 +0300

    Homework02 lesson1

---
+++ src/com/softserve/Homework1/Homework02.java
@@ -0,0 +1,20 @@
+package com.softservee.Homework1;
+import java.util.Scanner;
+
+public class Homework02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.print("Where do you live, " + name + "? ");
+        String address = scanner.nextLine();
+
+        System.out.println("Complete Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+
+        scanner.close();
+    }
+}
