1c8615b808d9cb24e1b7b22fd0070c2c84508056
Yurii Flys <flysyurii@gmail.com>
2023-06-26 02:49:12 +0300

    Practical Task 2

---
+++ src/com/softserve/practical/task2/Main.java
@@ -0,0 +1,19 @@
+package com.softserve.practical.task2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double sum = 0;
+        String[] prompts = {"Input First number: ", "Input Second number: ", "Input Third number: "};
+        for (int i = 0; i < 3; i++) {
+            System.out.print(prompts[i]);
+            sum += Double.parseDouble(reader.readLine());
+        }
+        double average = sum / 3;
+        System.out.println("Average is " + average);
+    }
+}
+
