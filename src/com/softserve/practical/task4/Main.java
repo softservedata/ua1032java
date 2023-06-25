9858cf9e9dc3b2043a17a5882282d2a7a35e5eb6
Yurii Flys <flysyurii@gmail.com>
2023-06-26 02:49:29 +0300

    Practical Task 4

---
+++ src/com/softserve/practical/task4/Main.java
@@ -0,0 +1,13 @@
+package com.softserve.practical.task4;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("How are you?");
+        String answer = reader.readLine();
+        System.out.println("You are " + answer);
+    }
+}
