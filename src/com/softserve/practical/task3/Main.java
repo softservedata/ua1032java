d4e02982f02d4b2b9deb9c7923c66d788beaa296
Yurii Flys <flysyurii@gmail.com>
2023-06-26 02:49:20 +0300

    Practical Task 3

---
+++ src/com/softserve/practical/task3/Main.java
@@ -0,0 +1,18 @@
+package com.softserve.practical.task3;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter a: ");
+        int a = Integer.parseInt(reader.readLine());
+        System.out.print("Enter b: ");
+        int b = Integer.parseInt(reader.readLine());
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
