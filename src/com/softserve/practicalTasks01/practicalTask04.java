718a555cfd23fcaff2474e65302966a10e781967
iamkachmar <kachmardev@gmail.com>
2023-06-26 16:59:17 +0300

    practical task 4

---
+++ src/com/softserve/practicalTasks01/practicalTask04.java
@@ -0,0 +1,16 @@
+package com.softserve.practicalTasks01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class practicalTask04 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("How are you?");
+        String answer = br.readLine();
+        System.out.println("You are " + answer);
+
+
+    }
+}
