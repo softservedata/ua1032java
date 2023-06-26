f36ad5b90cca50657bc446e30e92e845dd9004c1
Maksym <maks.smolinets1978@gmail.com>
2023-06-26 15:11:08 +0300

    homework_1 task_1

--- src/com/softserve/edu01/homeworks/Hw1_1.java
+++ src/com/softserve/edu01/homeworks/Hw1_1.java
@@ -1,4 +1,12 @@
 package com.softserve.edu01.homeworks;
 
 public class Hw1_1 {
+    public static void main(String[] args) {
+        String my = "my";
+        String first = "first";
+        String program = "program";
+
+        String result = program + "\n" + first + "\n" + my;
+        System.out.println();
+    }
 }
