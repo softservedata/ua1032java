e714a62d2bcca6373b3ffbb311d6c3b65cbcd889
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-06-26 23:41:10 +0300

    TriangleArea

--- src/com/softserve/homework/hw02/task1/Test.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.homework.hw02.task1;
-
-public class Test {
-    public static void main ( String[] args ) {
-        System.out.println("1");
-
-
-    }
-}
--- src/com/softserve/practicals/p2/task2/Test2.java
+++ src/com/softserve/practicals/p2/task2/Test2.java
@@ -2,6 +2,6 @@ package com.softserve.practicals.p2.task2;
 
 public class Test2 {
     public static void main ( String[] args ) {
-        System.out.println("2");
+        System.out.println("22");
     }
 }
