77d0429710348c38688e42a47ede265e2dbe8b68
iamkachmar <kachmardev@gmail.com>
2023-06-26 16:54:07 +0300

    practical task 1

---
+++ src/com/softserve/practicalTasks01/practicalTask01.java
@@ -0,0 +1,14 @@
+package com.softserve.practicalTasks01;
+
+public class practicalTask01 {
+    public static void main(String[] args) {
+        String firstWord = "my";
+        String secondWord = "first";
+        String thirdWord = "program";
+
+        System.out.println(thirdWord);
+        System.out.println(secondWord);
+        System.out.println(firstWord);
+
+    }
+}
