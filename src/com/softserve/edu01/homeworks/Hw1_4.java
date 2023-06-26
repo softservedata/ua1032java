9be32c6aad7bd01957f778010374418d7af3c874
Maksym <maks.smolinets1978@gmail.com>
2023-06-26 14:45:04 +0300

    homework_1 task_4

--- src/com/softserve/edu01/homeworks/Hw1_4.java
+++ src/com/softserve/edu01/homeworks/Hw1_4.java
@@ -1,4 +1,15 @@
 package com.softserve.edu01.homeworks;
+import java.util.Scanner;
 
 public class Hw1_4 {
+    public static void main(String[] args) {
+        Scanner task = new Scanner(System.in);
+
+        System.out.println("How are you?");
+        String answer = task.nextLine();
+
+        System.out.println("You are " + answer);
+
+        task.close();
+    }
 }
