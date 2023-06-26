d5618d663613a57f1485393c78f0d4e7c4d2365c
Maksym <maks.smolinets1978@gmail.com>
2023-06-26 14:44:05 +0300

    homework_1 task_3

--- src/com/softserve/edu01/homeworks/Hw1_3.java
+++ src/com/softserve/edu01/homeworks/Hw1_3.java
@@ -1,4 +1,35 @@
 package com.softserve.edu01.homeworks;
-
+import java.util.Scanner;
 public class Hw1_3 {
+    public static void main(String[] args) {
+        Scanner task = new Scanner(System.in);
+
+        System.out.println("Enter your variable for A: ");
+        int a = task.nextInt();
+        System.out.println("Enter your variable for A: ");
+        int b = task.nextInt();
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int division = a / b;
+
+        System.out.println("Press \"ENTER\" for sum");
+        task.nextLine();
+        System.out.println("Sum A and B: " + sum);
+
+        System.out.println("Press \"ENTER\" for difference");
+        task.nextLine();
+        System.out.println("Difference A and B: " + difference);
+
+        System.out.println("Press \"ENTER\" for product");
+        task.nextLine();
+        System.out.println("Product A and B: " + product);
+
+        System.out.println("Press \"ENTER\" for division");
+        task.nextLine();
+        System.out.println("Division A and B: " + division);
+
+        task.close();
+    }
 }
