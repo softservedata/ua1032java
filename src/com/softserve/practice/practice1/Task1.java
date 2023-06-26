e2c472370635201f4894c6c1282f9ad4bf05b3f2
Anton Nadieiko <nadieikoanton@gmail.com>
2023-06-26 16:57:15 +0300

    Practice tasks #1

--- src/com/softserve/practice/practice1/Task1.java
+++ src/com/softserve/practice/practice1/Task1.java
@@ -1,7 +1,21 @@
 package com.softserve.practice.practice1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task1 {
-    public static void main(String[] args)  {
-        System.out.println("HELLO");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Write down some words or sentences in the following lines:");
+        System.out.print("");
+        String a = br.readLine();
+        System.out.print("");
+        String b = br.readLine();
+        System.out.print("");
+        String c = br.readLine();
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a);
     }
 }
