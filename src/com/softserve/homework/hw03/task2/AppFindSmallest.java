c6e95ef796b6e1a08a7fe4329b48313c6bbc8ce2
Andriy Fedoryshyn <andriyfedoryshyn4@gmail.com>
2023-06-27 17:24:19 +0300

    need to be input and output static then fields is unable to read

---
+++ src/com/softserve/homework/hw03/task2/AppFindSmallest.java
@@ -0,0 +1,31 @@
+package com.softserve.homework.hw03.task2;
+
+import java.util.Scanner;
+
+public class AppFindSmallest {
+    public static void main(String[] args) {
+
+        int firstNumber;
+
+        int secondNumber;
+
+        int thirdNumber;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input first number: ");
+        firstNumber = sc.nextInt();
+
+        System.out.println("Input second number: ");
+        secondNumber = sc.nextInt();
+
+        System.out.println("Input third number: ");
+        thirdNumber = sc.nextInt();
+
+        TrioNumbers trioNumbers1 = new TrioNumbers(firstNumber, secondNumber, thirdNumber);
+
+        System.out.println(trioNumbers1);
+
+        System.out.println("The smallest is " + TrioNumbers.findSmallestNumber(trioNumbers1));
+
+    }
+}
