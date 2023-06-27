9eb047a27fad7bcf9204ff48f9a6a264fba5ac8d
Marian <mv.hrychak91@gmail.com>
2023-06-28 01:09:40 +0300

    lesson 1

---
+++ src/com/softserve/edu/homework/HomeWork02.java
@@ -0,0 +1,21 @@
+package com.softserve.edu.homework;
+
+import java.util.Scanner;
+
+public class HomeWork02 {
+    public static void main(String[] args){
+        String question1 = "What is your name?";
+        System.out.println(question1);
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        String question2 = "Where do you live" + " " + name+"?";
+        System.out.println(question2);
+
+        String address = sc.nextLine();
+        System.out.println("Name:" + " " + name + ".\nAddress:" + " " + address + ".");
+        sc.close();
+
+    }
+}
