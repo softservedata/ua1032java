e2c8a69c7b34fbc1acf54173c5d2896a60757a64
Marian <mv.hrychak91@gmail.com>
2023-06-27 23:48:36 +0300

    lesson 1

---
+++ src/com/softserve/edu/homework/HomeWork01/src/com/softserve/edu/homework01/HomeWork01.java
@@ -0,0 +1,19 @@
+package com.softserve.edu.homework.HomeWork01.src.com.softserve.edu.homework01;
+import java.util.Scanner;
+public class HomeWork01 {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("Input the radius circle");
+       double radius = sc.nextDouble();
+       double area = Math.PI * Math.pow(radius, 2);
+       double perimeter = 2 * Math.PI * radius;
+
+       System.out.printf("Area = %.2f", area);
+       System.out.printf("\nPerimeter = %.2f", perimeter);
+
+       sc.close();
+
+
+    }
+}
