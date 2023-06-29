fb3aeb7f066722bc8b00a2fbb669ba242b016ced
Augustin Gerasym <augustingerasym@gmail.com>
2023-06-29 20:48:37 +0300

    add practical task02, lesson01

---
+++ src/java_practical/lesson01/Task02.java
@@ -0,0 +1,21 @@
+package java_practical.lesson01;
+
+import java.util.Scanner;
+
+class Task02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double average = getAverage(scanner, "Enter a number: ");
+        scanner.close();
+        System.out.println("Average: " + average);
+    }
+
+    public static double getAverage(Scanner scanner, String prompt) {
+        double[] numbers = new double[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.print(prompt);
+            numbers[i] = scanner.nextDouble();
+        }
+        return Math.round(((numbers[0] + numbers[1] + numbers[2]) / 3) * 100.0) / 100.0;
+    }
+}
