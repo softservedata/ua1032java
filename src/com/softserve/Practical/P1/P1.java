147f11b8f70b606ea0f37cae302a95f5f0486943
Rostyslav <rostyslavpasternak@gmail.com>
2023-06-29 13:52:06 +0200

    Create Practical1: task3 and task4

--- src/com/softserve/Practical/P1/P1.java
+++ src/com/softserve/Practical/P1/P1.java
@@ -2,9 +2,11 @@ package com.softserve.Practical.P1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public class P1 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*
         //TASK 1
         System.out.println("Task 1");
         System.out.println("==========================");
@@ -22,6 +24,7 @@ public class P1 {
         //TASK 2
         System.out.println("Task 2");
         System.out.println("==========================");
+        System.out.println("Input Number: ");
 
         double a2= Double.parseDouble(br.readLine());
         double b2= Double.parseDouble(br.readLine());
@@ -30,6 +33,36 @@ public class P1 {
         double average = (a2+b2+c2)/3;
 
         System.out.println("Average: "+ average);
+*/
+        //TASK 3
+
+        System.out.println("First number: ");
+        int a3 = Integer.parseInt(br.readLine());
+
+        System.out.println("Arithmetic operators(+-*/): ");
+        String str = br.readLine();
+        System.out.println("Second number: ");
+        int b3 = Integer.parseInt(br.readLine());
+        if(str.contains("+")){
+            System.out.println(a3+" + "+b3+" = " +(a3+b3));
+        }
+        if(str.contains("-")){
+            System.out.println(a3+" - "+b3+" = " + (a3-b3));
+        }
+        if(str.contains("*")){
+            System.out.println(a3+" * "+b3+" = " +(a3*b3));
+        }
+        if(str.contains("/")){
+            System.out.println(a3+" / "+b3+" = " + (a3/b3));
+            System.out.println("Remainder: "+a3%b3);
+        }
+        //TASK4
+
+        System.out.println("How are you? ");
+        String answer = br.readLine();
+
+        System.out.println("You are "+ answer);
+
 
         br.close();
     }
