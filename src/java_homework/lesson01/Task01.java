7f2678cd9aa24d2ff510fa5abd64cf63b8e74398
Augustin Gerasym <augustingerasym@gmail.com>
2023-06-30 00:50:45 +0300

    add homework task01,lesson 1

---
+++ src/java_homework/lesson01/Task01.java
@@ -0,0 +1,26 @@
+package java_homework.lesson01;
+
+import java.util.Scanner;
+
+class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a radius: ");
+        double radius = scanner.nextDouble();
+        scanner.close();
+
+        double perimeter = getPerimeter(radius);
+        double area = getArea(radius);
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
+    }
+
+    public static double getPerimeter(double radius) {
+        return Math.round(2 * Math.PI * radius * 100.0) / 100.0;
+    }
+
+    public static double getArea(double radius) {
+        return Math.round(Math.PI * Math.pow(radius, 2) * 100.0) / 100.0;
+    }
+}
