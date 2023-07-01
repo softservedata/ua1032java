19b9cc898e0d2ca215c76afa515a917a37c19aba
Lev <levpredko@gmail.com>
2023-07-01 14:20:00 +0300

    updated the appearance

---
+++ src/com/softserve/edu/practical/Practical_tasks_2/Task_1/Task_1.java
@@ -0,0 +1,4 @@
+package com.softserve.edu.practical.Practical_tasks_2.Task_1;
+
+public class Task_1 {
+}
---
+++ src/com/softserve/edu/practical/Practical_tasks_2/Task_2/Task_2.java
@@ -0,0 +1,4 @@
+package com.softserve.edu.practical.Practical_tasks_2.Task_2;
+
+public class Task_2 {
+}
