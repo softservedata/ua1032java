feabb2323a5ffa932b1ffaa89200ec1d9baba1a4
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-07-02 11:15:56 +0300

    NameOfTheContinent

---
+++ src/com/softserve/practicals/p3/task3/Continent.java
@@ -0,0 +1,28 @@
+package com.softserve.practicals.p3.task3;
+
+public enum Continent {
+    ANTARCTICA("Antarctica"),
+    NORTH_AMERICA("North America"),
+    SOUTH_AMERICA("South America"),
+    EUROPE("Europe"),
+    AFRICA("Africa"),
+    ASIA("Asia"),
+    OCEANIA("Oceania");
+
+    private final String name;
+
+    Continent ( String name ) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    @Override
+    public String toString () {
+        return "Continent{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
