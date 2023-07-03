fe59ed63bb50073d25bf3526aec35e8753162a59
Augustin Gerasym <augustingerasym@gmail.com>
2023-07-03 23:27:46 +0300

    improoved Task01, homework lesson 2

--- src/com/softserve/java_homework/lesson02/Task01.java
+++ src/com/softserve/java_homework/lesson02/Task01.java
@@ -1,9 +1,20 @@
 package com.softserve.java_homework.lesson02;
 
+import java.util.Scanner;
+
 class Task01 {
     public static void main(String[] args) {
-        double area = Math.round(getArea(4.2, 5.6, 4.1) * 100.0) / 100.0;
-        System.out.println("Area: " + area);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input side 1: " );
+        double side1 = scanner.nextDouble();
+        System.out.print("Input side 2: " );
+        double side2 = scanner.nextDouble();
+        System.out.print("Input side 3: " );
+        double side3 = scanner.nextDouble();
+
+        double area = Math.round(getArea(side1, side2, side3) * 100.0) / 100.0;
+        System.out.println("The area of the triangle is: : " + area);
     }
 
     public static double getArea(double a, double b, double c) {
