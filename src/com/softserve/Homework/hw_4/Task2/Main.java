1c068fa2fb717f2be73b52a16a52649e410e28b4
Yurii Flys <flysyurii@gmail.com>
2023-07-03 06:12:55 +0300

    Task 2

---
+++ src/com/softserve/Homework/hw_4/Task2/Main.java
@@ -0,0 +1,20 @@
+package com.softserve.Homework.hw_4.Task2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter three integer numbers: ");
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+        int num3 = Integer.parseInt(reader.readLine());
+
+        int max = Math.max(num1, Math.max(num2, num3));
+        int min = Math.min(num1, Math.min(num2, num3));
+
+        System.out.println("Maximum value: " + max);
+        System.out.println("Minimum value: " + min);
+    }
+}
