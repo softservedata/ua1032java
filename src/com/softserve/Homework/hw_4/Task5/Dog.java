96dbe3d4e98a354626ed575d0715dc9254b6471f
Yurii Flys <flysyurii@gmail.com>
2023-07-03 06:13:48 +0300

    Task 5

---
+++ src/com/softserve/Homework/hw_4/Task5/Dog.java
@@ -0,0 +1,53 @@
+package com.softserve.Homework.hw_4.Task5;
+
+enum Breed {
+    GOLDEN_RETRIEVER("Golden Retriever"),
+    LABRADOR_RETRIEVER("Labrador Retriever"),
+    GERMAN_SHEPHERD("German Shepherd");
+
+    private final String name;
+
+    Breed(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Dog {
+    private String name;
+    private Breed breed;
+    private int age;
+
+    public Dog(String name, Breed breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/com/softserve/Homework/hw_4/Task5/Main.java
@@ -0,0 +1,25 @@
+package com.softserve.Homework.hw_4.Task5;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Buddy", Breed.GOLDEN_RETRIEVER, 5);
+        Dog dog2 = new Dog("Bella", Breed.LABRADOR_RETRIEVER, 7);
+        Dog dog3 = new Dog("Charlie", Breed.GERMAN_SHEPHERD, 6);
+
+        if (!dog1.getName().equals(dog2.getName()) && !dog1.getName().equals(dog3.getName()) && !dog2.getName().equals(dog3.getName())) {
+            System.out.println("There are no two dogs with the same name");
+        } else {
+            System.out.println("There are two dogs with the same name");
+        }
+
+        Dog oldestDog = dog1;
+        if (dog2.getAge() > oldestDog.getAge()) {
+            oldestDog = dog2;
+        }
+        if (dog3.getAge() > oldestDog.getAge()) {
+            oldestDog = dog3;
+        }
+
+        System.out.println("The oldest dog is " + oldestDog.getName() + " and its breed is " + oldestDog.getBreed().getName());
+    }
+}
