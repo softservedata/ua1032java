cbf3396f99fe1e716070636a4ca0a5fb1bc33dc8
Rostyslav <rostyslavpasternak@gmail.com>
2023-07-03 16:37:01 +0200

    Practical 4: task3

---
+++ src/com/softserve/Practical/P4/Continents.java
@@ -0,0 +1,15 @@
+package com.softserve.Practical.P4;
+
+public enum Continents {
+    ASIA,
+    AFRICA,
+    NORTH_AMERICA,
+    SOUNTH_AMERICA,
+    ANTARCTICA,
+    EUROPE,
+    AUSTRALIA;
+
+
+
+
+}
