3c5bb8fe504e51a2d4752663807d62ee62539c32
yakomura <yakomura1272@gmail.com>
2023-07-07 01:18:03 +0300

    homework03. task03*

---
+++ src/hm04/task01.java
@@ -0,0 +1,26 @@
+package hm04;
+
+import java.util.Scanner;
+
+public class task01 {
+    public static void main(String[]agrs){
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.print("enter the first float number:");
+        float number1 = scanner.nextFloat();
+        System.out.print("enter the second float number:");
+        float number2 = scanner.nextFloat();
+        System.out.print("enter the thirth float number:");
+        float number3 = scanner.nextFloat();
+        boolean allInRange = checkRange(number1)&&checkRange(number2)&&checkRange(number3);
+
+        if (allInRange) {
+            System.out.println("All numbers are within the range [-5, 5].");
+        }else{
+            System.out.println("At least one number is outside the range [-5, 5].");
+        }
+    }
+    private static boolean checkRange (float number){
+        return number >= -5 && number <= 5;
+    }
+}
