7b0ba2b502fab8d1219fd0d74ff19a4eada9e393
Lev <levpredko@gmail.com>
2023-07-06 13:44:12 +0300

    added HW5(task_4).

---
+++ src/com/softserve/edu/homework/homework5_arrays_loops/task_4/Random_number.java
@@ -0,0 +1,25 @@
+package com.softserve.edu.homework.homework5_arrays_loops.task_4;
+import java.util.Scanner;
+public class Random_number {
+    public static void main(String [] args){
+       Scanner input = new Scanner(System.in);
+       int min = -10;
+       int max = 20;
+       max -= min;
+       int a = (int)((Math.random() * ++max)) + min;
+       for (int i = 0; i < 20; i++){
+           System.out.print("Try to guess a number in the range [-10;20] : ");
+           int b = Integer.parseInt(input.nextLine());
+           if (b > a){
+               System.out.println("Too high, try again.\n");
+           }
+           else if(b < a){
+               System.out.println("Too low, try again.\n");
+           }
+           else {
+               System.out.println("Yay, you guessed a random number!!!");
+               break;
+           }
+       }
+    }
+}
