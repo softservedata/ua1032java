3e67bd3dcbd239763e955196f1276c6630c23842
Yaroslav <garasym@gmail.com>
2023-07-06 19:26:33 +0300

    OOP. Polymorphism

---
+++ src/com/softserve/edu07ab/ClassB.java
@@ -0,0 +1,33 @@
+package com.softserve.edu07ab;
+
+public class ClassB extends ClassA {
+    public double i = 1.1;
+
+    @Override
+    public void m1() {
+        System.out.println("ClassB, metod m1, i=" + i);
+        // super.m1();
+    }
+
+    @Override
+    public void m4() {
+        System.out.println("ClassB, metod m4, i = " + i);
+    }
+
+    // @Override
+    public void m5() {
+        System.out.println("ClassB, metod m5");
+    }
+
+    // @Override
+    public static void m6() {
+        System.out.println("ClassB, static metod m6");
+        ClassA.m6();
+    }
+
+    //private void m8() {
+    public void m8() {
+        System.out.println("ClassB, metod m8");
+        super.m8();
+    }
+}
---
+++ src/com/softserve/edu07ab/Worker.java
@@ -0,0 +1,5 @@
+package com.softserve.edu07ab;
+
+public interface Worker {
+	 int getSalary(); // public abstract
+}
