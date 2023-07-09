c0ca24c340932ecac6b9c2b64de87554f03f79fe
Lev <levpredko@gmail.com>
2023-07-09 14:01:51 +0300

    Rewrote HW5(task2). Wrote a test for HW5(task2)

--- src/com/softserve/edu/homework/homework5_arrays_loops/task_2/Do_While.java
+++ src/com/softserve/edu/homework/homework5_arrays_loops/task_2/Do_While.java
@@ -5,17 +5,35 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Do_While {
-    public static void main (String[] args)throws IOException {
-        Scanner input = new Scanner(System.in);
-        String again;
-        do {
-            System.out.print("Enter first number : ");
-            double x = Double.parseDouble(input.nextLine());
-            System.out.print("Enter second number : ");
-            double y = Double.parseDouble(input.nextLine());
-            System.out.println("Sum : " + (x+y));
-            System.out.println("Do you want to perform the operation again ? (Yes/No)  ");
-            again = input.nextLine();
-        }while(Objects.equals(again, "Yes"));
-    }
+   public static double Input() throws IOException {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter first number : ");
+      double x = Double.parseDouble(input.nextLine());
+      System.out.print("Enter second number : ");
+      double y = Double.parseDouble(input.nextLine());
+      System.out.print("Sum : ");
+      return x + y;
+   }
+   public static double Add(double x,double y){
+     return (x + y) ;
+   }
+   public static boolean Agree_to_continue() {
+      Scanner input = new Scanner(System.in);
+      boolean flag;
+      System.out.println("Do you want to perform the operation again ? (Yes/No)  ");
+      String answer = input.nextLine();
+      if(Objects.equals(answer, "Yes")){
+          System.out.println("Ok, let's go !");
+          flag =  true;
+      }
+      else if (Objects.equals(answer, "No")){
+          System.out.println("Goodbye :(");
+          flag =  false;
+      }
+      else {
+          System.out.println("Can you repeat, please ?");
+          flag = true;
+      }
+      return flag;
+   }
 }
