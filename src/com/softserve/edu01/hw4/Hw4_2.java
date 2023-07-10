bc60d3a2e51d9071024e7c6ceec4a66184e19bc9
Maksym <maks.smolinets1978@gmail.com>
2023-07-10 16:48:20 +0300

    Homework4_2 complete

---
+++ src/com/softserve/edu01/hw4/Hw4_2.java
@@ -0,0 +1,34 @@
+package com.softserve.edu01.hw4;
+
+import java.util.Scanner;
+
+public class Hw4_2 {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("Enter first integer number");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter second integer number");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter third integer number");
+        int num3 = scanner.nextInt();
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("Number 1 is MAX value");
+        } else if (num2 >= num1 && num2>= num3) {
+            System.out.println("Number 2 is MAX value");
+        } else if (num3 >= num1 && num3>= num2) {
+            System.out.println("Number 3 is MAX value");
+        }
+        if (num1 <= num2 && num1 <= num3) {
+            System.out.println("Number 1 is MIN value");
+        } else if (num2 <= num1 && num2 <= num3) {
+            System.out.println("Number 2 is MIN value");
+        } else if (num3 <= num1 && num3 <= num2) {
+            System.out.println("Number 2 is MIN value");
+        }
+
+    }
+}
