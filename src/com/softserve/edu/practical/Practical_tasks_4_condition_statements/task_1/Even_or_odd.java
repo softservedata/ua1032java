86a542b83d9af7028790c341e3e0be7f4c5a5252
Lev <levpredko@gmail.com>
2023-07-11 21:42:57 +0300

    Added Practical_tasks_4(task_1,task_2).

---
+++ src/com/softserve/edu/practical/Practical_tasks_4_condition_statements/task_1/Even_or_odd.java
@@ -0,0 +1,22 @@
+package com.softserve.edu.practical.Practical_tasks_4_condition_statements.task_1;
+
+import java.io.IOException;
+import java.util.Scanner;
+public class Even_or_odd {
+    public static void main(String[]args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[3];
+
+        for(int i = 0; i < numbers.length ; i++){
+            System.out.print("Enter a " + (i+1) + " number :" );
+            numbers[i] = Integer.parseInt(input.nextLine());
+        }
+        int  how_many = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                how_many++;
+            }
+        }
+        System.out.println("Number of even numbers : "+how_many);
+    }
+}
