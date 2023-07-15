9cd78381ecf6e0b46d3de6e85886c26cce040e29
Lev <levpredko@gmail.com>
2023-07-15 19:00:41 +0300

    Added HM_8.

---
+++ src/com/softserve/edu/homework/homework8_Nested_Inner_classes/task_1/FullName.java
@@ -0,0 +1,16 @@
+package com.softserve.edu.homework.homework8_Nested_Inner_classes.task_1;
+
+public class FullName {
+    private String firstName;
+    private String lastName;
+    public FullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+}
