509f736aef9e503f4b92282bfc4f4c78f2ba6e73
Yurii Flys <flysyurii@gmail.com>
2023-07-17 20:09:13 +0300

    Practical(Fourth Lesson)

---
+++ src/com/softserve/Practical/practical_4/task4_5/Product.java
@@ -0,0 +1,14 @@
+package com.softserve.Practical.practical_4.task4_5;
+
+public class Product {
+    String name;
+    double price;
+    int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
