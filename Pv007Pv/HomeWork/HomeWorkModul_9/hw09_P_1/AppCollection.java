17b4d7d4c720c9cbc032b09c17a772edda06070f
Pv007Pv <awd321321123@gmail.com>
2023-07-18 17:27:58 +0300

    Home Work Modul 09.

---
+++ Pv007Pv/HomeWork/HomeWorkModul_9/hw09_P_1/AppCollection.java
@@ -0,0 +1,137 @@
+package hw09_P_1;
+
+import java.util.*;
+
+public class AppCollection {
+    public static void main(String[] args) {
+
+        //Declare collection myCollection of 10 integers and fill it random.
+        List<Integer> myCollection = new ArrayList<>();
+        CreateCollection(myCollection); // Generate a collection of random numbers.
+
+        List<Integer> originalCollection = new ArrayList<>(myCollection); // Create a copy of the input list.
+
+        System.out.println("myCollection ArrayList: " + originalCollection);
+
+
+
+        // Swap the maximum and minimum elements in the list.
+        myCollection = new ArrayList<>(originalCollection); // Restore the original collection
+
+        Integer maxMyCollection = Collections.max(myCollection); // Find the maximum element
+        Integer minMyCollection = Collections.min(myCollection); // Find the minimum element
+
+        int indexMaxMyCollection = myCollection.indexOf(maxMyCollection); // Find the index of the maximum element
+        int indexMinMyCollection = myCollection.indexOf(minMyCollection); // Find the index of the minimum element
+
+        Collections.swap(myCollection, indexMaxMyCollection, indexMinMyCollection); // Swap the maximum and minimum elements
+
+        System.out.println("myCollection after swapping the minimum and maximum elements: " + myCollection);
+
+
+        //Insert a random three-digit number before the first negative element of the list
+        myCollection = new ArrayList<>(originalCollection); // Restore the original collection
+
+        Random random2 = new Random();
+        int randomNum = random2.nextInt(900) + 100; // Generate a random three-digit number
+        System.out.println("Random number to insert: " + randomNum);
+
+        for (int i = 0; i < myCollection.size(); i++) { // Iterate through the collection
+            if (myCollection.get(i) < 0) { // Check if the element is negative
+                //negativeIndex = i;
+                myCollection.add(i, randomNum); // Insert the random number at the negative element position
+                break;
+            }
+        }
+        System.out.println("myCollection after inserting a random number: " + myCollection);
+
+
+
+        //Insert a zero between all neighboring elements collection myCollection with different signs
+        myCollection = new ArrayList<>(originalCollection); // Restore the original collection
+
+        List<Integer> tempList = new ArrayList<>();
+        for (int i = 0; i < myCollection.size() - 1; i++) { // Iterate through the collection (except the last element)
+            int currentElement = myCollection.get(i);
+            int nextElement = myCollection.get(i + 1);
+
+            tempList.add(currentElement);
+            if ((currentElement < 0 && nextElement > 0) || (currentElement > 0 && nextElement < 0)) { // Check if the adjacent elements have opposite signs
+                tempList.add(0); // Insert 0 between elements of opposite signs
+            }
+        }
+        tempList.add(myCollection.get(myCollection.size() - 1)); // Add the last element of the collection to tempList
+
+        myCollection.clear();
+        myCollection.addAll(tempList); // Update myCollection with tempList
+
+        System.out.println("myCollection after inserting zeros: " + myCollection);
+
+
+
+        /*Copy the first k elements of the myCollection to the list1, in direct order, and the rest to the
+        list2 in reverse order.*/
+        myCollection = new ArrayList<>(originalCollection); // Restore the original collection
+
+        int k = 5; // Number of elements to copy
+        List<Integer> list1 = new ArrayList<>(myCollection.subList(0, k)); // Copy the first k elements to list1
+
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = myCollection.size() - 1; i >= k; i--) { // Iterate through the collection in reverse order starting from the kth element
+            list2.add(myCollection.get(i)); // Copy the remaining elements in reverse order to list2
+        }
+
+        System.out.println("Copied first " + k +
+                " elements of myCollection to list1 in direct order: " + list1);
+        System.out.println("The rest copied to list2 in reverse order: " + list2);
+
+
+        /*In a list myCollection remove the last even element (if there are even elements in the list). If
+        there is no such element, display a message.*/
+        myCollection = new ArrayList<>(originalCollection); // Restore the original collection
+
+        boolean removed = false;
+        for (int i = myCollection.size() - 1; i >= 0; i--) { // Iterate through the collection in reverse order
+            if (myCollection.get(i) % 2 == 0) { // Check if the element is even
+                myCollection.remove(i); // Remove the last even element from myCollection
+                removed = true;
+                break;
+            }
+        }
+
+        if (removed) {
+            System.out.println("Last even element removed from myCollection: " + myCollection);
+        } else {
+            System.out.println("There are no paired elements");
+        }
+
+        System.out.println("myCollection after finding and deleting the last paired element: " + myCollection);
+
+
+
+        /*Remove from the list myCollection the element following the first minimum. If the minimum
+        element is the last one, nothing needs to be removed.*/
+        myCollection = new ArrayList<>(originalCollection); // Restore the original collection
+
+        int minMyCollection2 = Collections.min(myCollection); // Find the minimum element
+        int indexMinMyCollection2 = myCollection.indexOf(minMyCollection2); // Find the index of the first minimum element
+
+        if (indexMinMyCollection2 < myCollection.size() - 1) { // Check if there is a next element
+            myCollection.remove(indexMinMyCollection2 + 1); // Remove the element following the first minimum
+            System.out.println("The element after the first minimum has been removed from myCollection: " + myCollection);
+        } else {
+            System.out.println("The minimum element is the last one, nothing is removed");
+        }
+
+        System.out.println("myCollection after removing the following from the first minimum: " + myCollection);
+    }
+
+    //The method fills the Collection with random numbers from -100 to 100.
+    private static void CreateCollection(List<Integer> Collection) {
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int number = random.nextInt(201) - 100; // Generate a random number in the range from -100 to 100
+            Collection.add(number); // Add the number to the collection
+        }
+    }
+}
---
+++ Pv007Pv/HomeWork/HomeWorkModul_9/hw09_P_2/Student.java
@@ -0,0 +1,140 @@
+package hw09_P_2;
+/**
+ * 2. Create a class called Student which includes the following fields: name, group, course, and
+ grades in different subjects. Create a collection that holds all objects. Write a methods that:
+ o removes students with a grade point average of less than 3. If a student's average score is
+ 3 or higher, then they will be automatically promoted to the next course level.
+ o printStudents(List<Student> students, int course) which takes a list of students and a
+ course number as inputs. This method should print out the names of the students who
+ are enrolled in the specified course number to the console.
+ • In main() method create collection and output result.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+//The Student class represents a student with their name, group, course, and average grade.
+public class Student {
+    private String name;                // Student's name
+    private String group;               // Student's group
+    private int course;                 // Student's course
+    private double averageGrade;        // Student's average grade
+
+    public Student(String name, String group, int course, double averageGrade) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.averageGrade = averageGrade;
+    }
+
+    // Getters and Setters for the fields
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+
+        // Create student objects
+        Student student1 = new Student("Ivan", "MI-1", 1, 2);
+        Student student2 = new Student("Petro", "MI-2", 2, 2);
+        Student student3 = new Student("Misha", "MI-3", 3, 4.2);
+        Student student4 = new Student("Lyda", "MI-1", 1, 3.9);
+
+        // Add students to the list
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+
+        // Print details of all students
+        System.out.println("All Students:");
+        for (Student student : studentList) {
+            System.out.println("Name: " + student.getName()
+                    + "; Group: " + student.getGroup()
+                    + "; Course: " + student.getCourse()
+                    + "; Average Grade: " + student.getAverageGrade() + ".");
+            System.out.println();
+        }
+
+        // Print students of a specific course
+        int courseNumber = 2;
+        printStudents(studentList, courseNumber);
+
+        // Update student positions (expel or promote)
+        positionOfTheStudent(studentList);
+
+        // Print details of students after position update
+        System.out.println("Students transferred to the next course:");
+        for (Student student : studentList) {
+            System.out.println("Name: " + student.getName()
+                    + "; Group: " + student.getGroup()
+                    + "; Course: " + student.getCourse()
+                    + "; Average Grade: " + student.getAverageGrade() + ".");
+            System.out.println();
+        }
+    }
+
+    //Updates the position (course) of each student based on their average grade.
+    public static void positionOfTheStudent(List<Student> studentList) {
+        List<Student> expelledStudents = new ArrayList<>();
+        for (Student student : studentList) {
+            if (student.getAverageGrade() < 3) {
+                expelledStudents.add(student);
+            } else {
+                int currentCourse = student.getCourse();
+                student.setCourse(currentCourse + 1);
+            }
+        }
+        studentList.removeAll(expelledStudents);
+
+        System.out.println("Expelled Students: ");
+        for (Student expelledStudent : expelledStudents) {
+            System.out.println(expelledStudent.getName());
+        }
+        System.out.println();
+    }
+
+    //Prints the names of students in a specific course.
+     public static void printStudents(List<Student> studentList, int course) {
+        System.out.println("Students in Course " + course + ": ");
+        for (Student student : studentList) {
+            if (student.getCourse() == course) {
+                System.out.println(student.getName());
+            }
+        }
+    }
+}
+
+
+
+
