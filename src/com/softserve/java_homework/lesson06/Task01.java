3405233dfe3952dce9e25b9923f717d03ca1da95
Augustin Gerasym <augustingerasym@gmail.com>
2023-07-19 17:44:58 +0300

    add task01, homework lesson06

--- src/com/softserve/java_homework/lesson06/Task01.java
+++ src/com/softserve/java_homework/lesson06/Task01.java
@@ -2,6 +2,17 @@ package com.softserve.java_homework.lesson06;
 
 public class Task01 {
     public static void main(String[] args) {
+        Bird[] birds = new Bird[4];
 
+        birds[0] = new Eagle(true, true);
+        birds[1] = new Swallow(true, true);
+        birds[2] = new Penguin(true, true);
+        birds[3] = new Kiwi(true, true);
+
+        for (Bird bird : birds) {
+            System.out.println(bird.getClass().getSimpleName() + ": " + bird);
+            bird.fly();
+            System.out.println();
+        }
     }
 }
