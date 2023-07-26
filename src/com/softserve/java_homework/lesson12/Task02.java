fbbf8331d84abd1ef3bdcdfc8b77b5a77d28df2c
Augustin Gerasym <augustingerasym@gmail.com>
2023-07-27 00:34:05 +0300

    improoved task02, homework lesson12

--- src/com/softserve/java_homework/lesson12/Task02.java
+++ src/com/softserve/java_homework/lesson12/Task02.java
@@ -3,24 +3,26 @@ package com.softserve.java_homework.lesson12;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Task02 {
         static Scanner scanner = new Scanner(System.in);
+        private static final int START = 1;
+        private static final int END = 100;
+        private static final int MAX_ATTEMPTS = 10;
+        private static final Pattern PATTERN = Pattern.compile("\\d+");
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
-        int start = 1;
-        int end = 100;
-        for (int i = 0; i < 10; i++) {
+        int currentStart = START;
+        int count = 0;
+        while (count < MAX_ATTEMPTS) {
             try {
-                int number = readNumber(start, end, scanner);
-                while (number <= start || number >= end) {
-                    System.out.println("Number is out of range.");
-                    number = readNumber(start, end, scanner);
-                }
+                int number = readNumber(currentStart, END);
                 numbers.add(number);
-                start = number;
-                if (start == end - 1) {
-                    System.out.println("There are no numbers left between " + start + " and " + end);
+                currentStart = number;
+                count++;
+                if (currentStart >= END - 1) {
+                    System.out.println("There are no numbers left between " + currentStart + " and " + END);
                     break;
                 }
             } catch (NumberFormatException e) {
@@ -30,17 +32,21 @@ public class Task02 {
         System.out.println("Result: " + numbers);
     }
 
-    public static int readNumber(int start, int end, Scanner scanner) {
-        String p = "\\d+";
+    public static int readNumber(int start, int end) {
         int number;
         System.out.println("Enter a number bigger than " + start + ", and less than " + end + ":");
-        System.out.print("Enter: ");
-        String input = scanner.nextLine();
-        if (!input.matches(p)) {
-            throw new NumberFormatException("Invalid input. The value must be an positive integer.");
-        } else {
-            number = Integer.parseInt(input);
-        }
+        do {
+            System.out.print("Enter: ");
+            String input = scanner.nextLine();
+            if (!PATTERN.matcher(input).matches()) {
+                throw new NumberFormatException("Invalid input. The value must be an positive integer.");
+            } else {
+                number = Integer.parseInt(input);
+            }
+            if (number <= start || number >= end) {
+                System.out.println("The number is out of range.");
+            } else break;
+        } while (true);
         return number;
     }
 }
