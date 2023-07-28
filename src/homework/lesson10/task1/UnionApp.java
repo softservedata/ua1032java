2d9cd4c2bc99ea203790985ae702ebddc437db59
Olexandr Mozil <sashamozil@gmail.com>
2023-07-28 19:11:22 +0300

    changed file UnionApp (added intersection)

--- src/homework/lesson10/task1/UnionApp.java
+++ src/homework/lesson10/task1/UnionApp.java
@@ -1,8 +1,8 @@
 package homework.lesson10.task1;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class UnionApp {
 
@@ -11,6 +11,19 @@ public class UnionApp {
         united.addAll(set1);
         united.addAll(set2);
         return united;
+    }
+    public static Set<Integer> intersection (Set<Integer> set1, Set<Integer> set2){
+        Set<Integer> intersected = new HashSet<>();
+
+        Iterator<Integer> it = set1.iterator();
+        while(it.hasNext()){
+            int element = it.next();
+            if(set2.contains(element)){
+                intersected.add(element);
+            }
+        }
+
+        return intersected;
     }
 
     public static void main(String[] args) {
@@ -25,5 +38,6 @@ public class UnionApp {
         System.out.println("Set1: " + set1);
         System.out.println("Set2: " + set2);
         System.out.println("Using union method: " + union(set1, set2));
+        System.out.println("Using intersection method: " + intersection(set1, set2));
     }
 }
