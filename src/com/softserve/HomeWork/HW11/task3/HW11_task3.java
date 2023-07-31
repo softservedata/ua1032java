be1e83e6a147a731a0012d5c468484b3d80a3eed
Rostyslav <rostyslavpasternak@gmail.com>
2023-07-31 16:12:09 +0200

    Homework 11: task3

--- src/com/softserve/HomeWork/HW11/task3/HW11_task3.java
+++ src/com/softserve/HomeWork/HW11/task3/HW11_task3.java
@@ -7,14 +7,14 @@ import java.util.regex.Pattern;
 public class HW11_task3 {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String form = "(\\$)[\\d]+|[.,](\\d{2})";
         System.out.print("Number: ");
-        String currency = sc.nextLine();
+        String currency = scanner.nextLine();
 
         Pattern p = Pattern.compile(form);
         Matcher m = p.matcher(currency);
         System.out.println("Found: ");
+
         while (m.find())
             System.out.print(currency.substring(m.start(), m.end()));
     }
