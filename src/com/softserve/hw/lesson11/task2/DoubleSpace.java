af648e631b27b66455bcdf5b308941e1d7437da1
OlhenShu <olya.shutileva@gmail.com>
2023-08-02 00:29:03 +0300

    added comments

--- src/com/softserve/hw/lesson11/task2/DoubleSpace.java
+++ src/com/softserve/hw/lesson11/task2/DoubleSpace.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class DoubleSpace {
     public static String reverseOneSpace(String sentence) {
         String[] split = sentence.split("\\s+");
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();//StringBuilder is more quickly than StringBuffer, but it is not thread-safe
+        //but in this case we don't need thread-safe
         String result = null;
         for (int i = 0; i < split.length; i++) {
             result = String.valueOf(sb
