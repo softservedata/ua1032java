cf935bef95212466302f72c06ffc39344500cd2a
Augustin Gerasym <augustingerasym@gmail.com>
2023-08-03 16:30:05 +0300

    add task03, homework lesson13

---
+++ src/com/softserve/java_homework/lesson13/Task03.java
@@ -0,0 +1,27 @@
+package com.softserve.java_homework.lesson13;
+
+import java.time.LocalDate;
+
+public class Task03 {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        int year1 = 2020;
+        int year2 = 2022;
+
+        System.out.println("Year " + currentDate.getYear() + " is leap: " + isLeapYear(currentDate.getYear()));
+        System.out.println("Year " + year1 + " is leap: " + isLeapYear(year1));
+        System.out.println("Year " + year2 + " is leap: " + isLeapYear(year2));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
