b1095a364b02e51e2bf7f6beee8b1845977e003c
Maksym <maks.smolinets1978@gmail.com>
2023-08-08 12:59:32 +0300

    Homework 11, tasks 1,2 complete

---
+++ src/com/softserve/edu01/hw11/task3/Dollar.java
@@ -0,0 +1,25 @@
+package com.softserve.edu01.hw11.task3;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Dollar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text containing US currency format: ");
+        String inputText = scanner.nextLine();
+        scanner.close();
+
+        String currencyPattern = "\\$\\d+(\\.\\d{2})?";
+        Pattern pattern = Pattern.compile(currencyPattern);
+        Matcher matcher = pattern.matcher(inputText);
+
+        System.out.println("US Currency format occurrences:");
+        while (matcher.find()) {
+            String currencyMatch = matcher.group();
+            System.out.println(currencyMatch);
+        }
+    }
+}
