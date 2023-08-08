b1636f97f0b1d6ab9ecb5d7e6ada0bc84f1eb21d
yakomura <yakomura1272@gmail.com>
2023-08-08 03:47:57 +0300

    homework14. task01.java

--- src/hm14/task01.java
+++ src/hm14/task01.java
@@ -1,74 +1,91 @@
 package hm14;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
+import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-class Main {
-    public static class Product {
-        private String name;
-        private String category;
-        private LocalDate manufactureDate;
-        private double price;
+class Product {
+    private String manufactureCategory;
+    private LocalDate dateOfManufacture;
+    private double price;
 
-        public Product(String name, String category, LocalDate manufactureDate, double price) {
-            this.name = name;
-            this.category = category;
-            this.manufactureDate = manufactureDate;
-            this.price = price;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getCategory() {
-            return category;
-        }
-
-        public LocalDate getManufactureDate() {
-            return manufactureDate;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public boolean isPhone() {
-            return category.equals("Phone");
-        }
-
-        public boolean isPriceOver3000() {
-            return price > 3000;
-        }
-
-        public boolean isManufacturedOverYearAgo() {
-            return manufactureDate.isBefore(LocalDate.now().minusYears(1));
-        }
+    public Product(String manufactureCategory, LocalDate dateOfManufacture, double price) {
+        this.manufactureCategory = manufactureCategory;
+        this.dateOfManufacture = dateOfManufacture;
+        this.price = price;
     }
 
-    public static void main(String[] args) {
-        List<Product> products = new ArrayList<>();
+    public String getManufactureCategory() {
+        return manufactureCategory;
+    }
 
-        List<Product> sortedProducts = products.stream()
-                .filter(Product::isPhone)
-                .filter(Product::isPriceOver3000)
-                .filter(Product::isManufacturedOverYearAgo)
-                .sorted(Comparator.comparingDouble(Product::getPrice))
+    public void setManufactureCategory(String manufactureCategory) {
+        this.manufactureCategory = manufactureCategory;
+    }
+
+    public LocalDate getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public void setDateOfManufacture(LocalDate dateOfManufacture) {
+        this.dateOfManufacture = dateOfManufacture;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 &&
+                Objects.equals(manufactureCategory, product.manufactureCategory) &&
+                Objects.equals(dateOfManufacture, product.dateOfManufacture);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(manufactureCategory, dateOfManufacture, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product: " +
+                "manufactureCategory= " + manufactureCategory +
+                ", dateOfManufacture= " + dateOfManufacture +
+                ", price= " + price + "\n";
+    }
+
+    public static List<Product> seekProductByPriceAndYears(List<Product> products) {
+        return products.stream()
+                .filter(product -> product.getPrice() > 3000)
+                .filter(product -> product.getManufactureCategory().equalsIgnoreCase("Smartphone"))
+                .filter(product -> product.getDateOfManufacture().isBefore(LocalDate.now().minusYears(1)))
+                .sorted(Comparator.comparing(Product::getPrice))
                 .collect(Collectors.toList());
-
-        System.out.println("Sorted products by categories Phone with a price of more than 3000 and manufactured more than a year ago:");
-        sortedProducts.forEach(product ->
-                System.out.println(product.getName() + " - " + product.getPrice()));
     }
 
-    static Optional<String> mostPopularName(Stream<Employee> employees) {
-        return null;
-    }
-
-    public static class Employee {
+    public static List<Product> createList() {
+        List<Product> result = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            result.add(new Product("Laptop",
+                    LocalDate.now().minusYears(new Random().nextInt(5)),
+                    new Random().nextDouble(2500.0, 4000.0)));
+            result.add(new Product("Smartphone",
+                    LocalDate.now().minusYears(new Random().nextInt(5)),
+                    new Random().nextDouble(2500.0, 4000.0)));
+        }
+        return result;
     }
 }
+
