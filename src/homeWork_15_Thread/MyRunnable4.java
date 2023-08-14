68add0075b48f577632039d16bc1291a78c4fe15
Svyatoslav Duma <svytoslav.duma07@gmail.com>
2023-08-15 00:44:30 +0300

    ADD HOME_WORK 15 thread

--- src/homeWork_15_Thread/MyRunnable4.java
+++ src/homeWork_15_Thread/MyRunnable4.java
@@ -2,7 +2,7 @@ package homeWork_15_Thread;
 
 public class MyRunnable4 extends Thread{
     @Override
-    public void run() {
+    public void  run() {
         for (int i =0; i<5; i++){
             System.out.println(4);
         }
